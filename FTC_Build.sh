4f42cffbe65800827027b4e242751597271ec503
Brooks J Rady <b.j.rady@gmail.com>
2017-10-31 17:00:59 -0600

    Fixed strange ghost bug when compiling

--- FTC_Build.sh
+++ FTC_Build.sh
@@ -2,6 +2,10 @@
 
 ROOT=192.168.49.1:8080
 
+# This seems to update some sort of file index in the background and is required
+# for the build to succeed after adding or removing files.
+wget -qO /dev/null $Root/java/file/tree
+
 wget -qO /dev/null $ROOT/java/build/start
 
 echo -n "Building"
